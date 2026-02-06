b60e4ec03dbf39c905615e62d426e087898918cd
Semih Enser <semihenser@gmail.com>
2026-02-06 10:52:31 +0300

    env fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // GitHub Pages alt klasöründe çalışabilmesi için relative path
-  build: {
-    outDir: 'dist',
-  }
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: './', // GitHub Pages relative path
+    build: {
+      outDir: 'dist',
+    },
+    define: {
+      // Stringify the env object so it's injected as a proper object literal
+      'process.env': JSON.stringify(env)
+    }
+  };
 });
